ea2e5f958847c3ef632ad6d1c5c0e1b31bf376b7
Chizoba Obasi <40410289+chizkidd@users.noreply.github.com>
2024-11-12 20:27:47 -0600

    Update app.py file to ensure flask is running

--- app.py
+++ app.py
@@ -33,5 +33,5 @@ def predict():
     return jsonify({'prediction': species_name})
 
 # Run the Flask app (make sure to use gunicorn in production)
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
