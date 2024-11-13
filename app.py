2d4034e9ec47ec7f873f434bba1d6f7f7700991f
Chizoba Obasi <40410289+chizkidd@users.noreply.github.com>
2024-11-12 21:13:21 -0600

    Update app.py, Procfile

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def predict():
 
 # Run the Flask app (make sure to use gunicorn in production)
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
